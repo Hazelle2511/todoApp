f524bd60751909b6cc7c0b949e350604e7bb125f
Hazelle2511 <camacho.hazelle@gmail.com>
2022-09-18 23:20:22 +0200

    Updates

---
+++ precache-manifest.06e2022be4788d59a289cf7700439aa2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e8558d5d0ece28f2e02",
+    "url": "/todoApp/static/css/main.8547b491.chunk.css"
+  },
+  {
+    "revision": "1e8558d5d0ece28f2e02",
+    "url": "/todoApp/static/js/main.43160e9f.chunk.js"
+  },
+  {
+    "revision": "7ffb180d2dcaa0b59a98",
+    "url": "/todoApp/static/js/runtime~main.1d50937c.js"
+  },
+  {
+    "revision": "1a2fa331586e836ced41",
+    "url": "/todoApp/static/css/2.0b60a7f9.chunk.css"
+  },
+  {
+    "revision": "1a2fa331586e836ced41",
+    "url": "/todoApp/static/js/2.4b70eaab.chunk.js"
+  },
+  {
+    "revision": "bbae93786a029de7b9458f5ca63e01fd",
+    "url": "/todoApp/static/media/poppins-all-400-normal.bbae9378.woff"
+  },
+  {
+    "revision": "0ed299a4bb5262e17e2145783b2c18f1",
+    "url": "/todoApp/static/media/poppins-latin-ext-400-normal.0ed299a4.woff2"
+  },
+  {
+    "revision": "790d0c8dbcd491d29d58f1369c199d40",
+    "url": "/todoApp/static/media/poppins-devanagari-400-normal.790d0c8d.woff2"
+  },
+  {
+    "revision": "9212f6f9860f9fc6c69b02fedf6db8c3",
+    "url": "/todoApp/static/media/poppins-latin-400-normal.9212f6f9.woff2"
+  },
+  {
+    "revision": "a09f2fccfee35b7247b08a1a266f0328",
+    "url": "/todoApp/static/media/poppins-latin-500-normal.a09f2fcc.woff2"
+  },
+  {
+    "revision": "79e6e81d255edac7e8627c7e16baccf5",
+    "url": "/todoApp/static/media/poppins-devanagari-500-normal.79e6e81d.woff2"
+  },
+  {
+    "revision": "a6ff41d10fa89e7f8fec937c243d7428",
+    "url": "/todoApp/static/media/poppins-latin-ext-500-normal.a6ff41d1.woff2"
+  },
+  {
+    "revision": "3bb9ab2862ae072e657fb2fb2b6a3279",
+    "url": "/todoApp/static/media/poppins-all-500-normal.3bb9ab28.woff"
+  },
+  {
+    "revision": "b6eb345b4e0cb02a9d332aa5145c3c2b",
+    "url": "/todoApp/static/media/poppins-all-600-normal.b6eb345b.woff"
+  },
+  {
+    "revision": "e9fd398a43c9e51f9ee14e757eaf95d9",
+    "url": "/todoApp/static/media/poppins-devanagari-600-normal.e9fd398a.woff2"
+  },
+  {
+    "revision": "72b3ae37567ee5efdf2254b657c36ba9",
+    "url": "/todoApp/static/media/poppins-latin-ext-600-normal.72b3ae37.woff2"
+  },
+  {
+    "revision": "72993dddf88a63e8f226656f7de88e57",
+    "url": "/todoApp/static/media/poppins-latin-600-normal.72993ddd.woff2"
+  },
+  {
+    "revision": "25b0e113ca7cce3770d542736db26368",
+    "url": "/todoApp/static/media/poppins-latin-700-normal.25b0e113.woff2"
+  },
+  {
+    "revision": "7def222d1a45cb1cb7d8c3ae675dbdcc",
+    "url": "/todoApp/static/media/poppins-devanagari-700-normal.7def222d.woff2"
+  },
+  {
+    "revision": "96b6d54684daa94742f7bfd72a981213",
+    "url": "/todoApp/static/media/poppins-latin-ext-700-normal.96b6d546.woff2"
+  },
+  {
+    "revision": "4428e6c8c52a6f314021260c812f9c0a",
+    "url": "/todoApp/static/media/poppins-all-700-normal.4428e6c8.woff"
+  },
+  {
+    "revision": "a903b809d5cd6f4c2813ddac5a4444a5",
+    "url": "/todoApp/index.html"
+  }
+];
